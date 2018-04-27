a79a222465d06591befd3a8369f953e93937a664
Andrea Edstrom <aedstrom@mpr.org>
2018-04-27 16:47:36 -0500

    add latest

--- layout/js/main.js
+++ layout/js/main.js
@@ -34,3 +34,11 @@ $('a[href*="#"]')
       }
     }
   });
+
+  //toggle buttons
+  const toggle = document.querySelector('[aria-pressed]');
+
+  toggle.addEventListener('click', (e) => {
+    let pressed = e.target.getAttribute('aria-pressed') === 'true';
+    e.target.setAttribute('aria-pressed', String(!pressed));
+  });
